4c4aff24c28bb602c74e242e3e475f2c3d16ee8b
unknown <juls.ablis022@gmail.com>
2026-04-01 04:11:49 +0800

    fixed cors

--- src/plugins/cors.js
+++ src/plugins/cors.js
@@ -3,7 +3,10 @@ const cors = require("@fastify/cors");
 
 async function corsPlugin(app) {
   await app.register(cors, {
-    origin: true,
+    origin: [
+      "http://localhost:5173",
+      "https://main.d1nmregmm9djjc.amplifyapp.com"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
